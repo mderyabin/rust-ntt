9d28e82f88c1a31c80cea6657200c4fdb5246553
oiwn <alex@imscraping.ninja>
2025-07-09 15:55:20 +0700

    remove license checks, make clippy happy

--- src/polymath.rs
+++ src/polymath.rs
@@ -1,35 +1,39 @@
-// Number Theoretic operations over polynomials and vectors 
+// Number Theoretic operations over polynomials and vectors
 use crate::ntmath::*;
-use rand::{rng, Rng};
-
+use rand::{Rng, rng};
 
 #[derive(Debug, Clone, Copy)]
 pub struct PolyRing {
     pub class: CongruenceClass,
-    pub N: usize,
+    pub n: usize,
 }
 
 impl PolyRing {
-    pub fn new(q: u64, N: usize) -> Self {
+    pub fn new(q: u64, n: usize) -> Self {
         let class = CongruenceClass::new(q);
-        Self { class, N }
+        Self { class, n }
     }
 
     pub fn sample_random(&self) -> Vec<u64> {
         let mut generator = rng();
-        (0..self.N).map(|_| generator.random_range(1..self.class.q)).collect()
+        (0..self.n)
+            .map(|_| generator.random_range(1..self.class.q))
+            .collect()
     }
 
     pub fn add(&self, ax: &[u64], bx: &[u64]) -> Vec<u64> {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
-        assert_eq!(bx.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
+        assert_eq!(bx.len(), self.n, "input must have len = {}", self.n);
 
-        ax.iter().zip(bx).map(| (&a, &b) | self.class.modadd(a, b) ).collect()
+        ax.iter()
+            .zip(bx)
+            .map(|(&a, &b)| self.class.modadd(a, b))
+            .collect()
     }
 
     pub fn add_eq(&self, ax: &mut [u64], bx: &[u64]) {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
-        assert_eq!(bx.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
+        assert_eq!(bx.len(), self.n, "input must have len = {}", self.n);
 
         for (a, &b) in ax.iter_mut().zip(bx) {
             self.class.modadd_eq(&mut *a, b);
@@ -37,15 +41,18 @@ impl PolyRing {
     }
 
     pub fn sub(&self, ax: &[u64], bx: &[u64]) -> Vec<u64> {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
-        assert_eq!(bx.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
+        assert_eq!(bx.len(), self.n, "input must have len = {}", self.n);
 
-        ax.iter().zip(bx).map(| (&a, &b) | self.class.modsub(a, b) ).collect()
+        ax.iter()
+            .zip(bx)
+            .map(|(&a, &b)| self.class.modsub(a, b))
+            .collect()
     }
 
     pub fn sub_eq(&self, ax: &mut [u64], bx: &[u64]) {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
-        assert_eq!(bx.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
+        assert_eq!(bx.len(), self.n, "input must have len = {}", self.n);
 
         for (a, &b) in ax.iter_mut().zip(bx) {
             self.class.modsub_eq(&mut *a, b);
@@ -53,13 +60,13 @@ impl PolyRing {
     }
 
     pub fn neg(&self, ax: &[u64]) -> Vec<u64> {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
 
-        ax.iter().map(| &a| self.class.modneg(a) ).collect()
+        ax.iter().map(|&a| self.class.modneg(a)).collect()
     }
 
     pub fn neg_eq(&self, ax: &mut [u64]) {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
 
         for a in ax.iter_mut() {
             self.class.modneg_eq(&mut *a);
@@ -67,15 +74,18 @@ impl PolyRing {
     }
 
     pub fn mul(&self, ax: &[u64], bx: &[u64]) -> Vec<u64> {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
-        assert_eq!(bx.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
+        assert_eq!(bx.len(), self.n, "input must have len = {}", self.n);
 
-        ax.iter().zip(bx).map(| (&a, &b) | self.class.modmul(a, b) ).collect()
+        ax.iter()
+            .zip(bx)
+            .map(|(&a, &b)| self.class.modmul(a, b))
+            .collect()
     }
 
     pub fn mul_eq(&self, ax: &mut [u64], bx: &[u64]) {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
-        assert_eq!(bx.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
+        assert_eq!(bx.len(), self.n, "input must have len = {}", self.n);
 
         for (a, &b) in ax.iter_mut().zip(bx) {
             self.class.modmul_eq(&mut *a, b);
@@ -83,22 +93,24 @@ impl PolyRing {
     }
 
     pub fn naive_negacyclic_convolution(&self, ax: &[u64], bx: &[u64]) -> Vec<u64> {
-        assert_eq!(ax.len(), self.N, "input must have len = {}", self.N);
-        assert_eq!(bx.len(), self.N, "input must have len = {}", self.N);
+        assert_eq!(ax.len(), self.n, "input must have len = {}", self.n);
+        assert_eq!(bx.len(), self.n, "input must have len = {}", self.n);
 
-        let mut cx= Vec::with_capacity(self.N+1);
+        let mut cx = Vec::with_capacity(self.n + 1);
 
-        for i in 0..self.N {
+        for i in 0..self.n {
             cx.push(0);
 
             for j in 0..=i {
-                self.class.modadd_eq(&mut cx[i], self.class.modmul(ax[j], bx[i-j]));
+                self.class
+                    .modadd_eq(&mut cx[i], self.class.modmul(ax[j], bx[i - j]));
             }
-            for j in (i+1)..self.N {
-                self.class.modsub_eq(&mut cx[i], self.class.modmul(ax[j], bx[self.N+i-j]));
+            for j in (i + 1)..self.n {
+                self.class
+                    .modsub_eq(&mut cx[i], self.class.modmul(ax[j], bx[self.n + i - j]));
             }
         }
 
         cx
-    } 
+    }
 }
